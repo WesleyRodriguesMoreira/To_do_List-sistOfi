f2153dbf9df7816105357d8e32d5e9800d52829d
Wesley Rodrigues <wesleyrodriguesmoreira02@gmail.com>
2024-08-31 18:21:20 -0300

    2° commit

--- to_do_react/src/App.jsx
+++ to_do_react/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 import './App.css';
 
 
-
 function App() {
+  // Dados para visualização | cria um arry em formato de JSON
   const [todos, setTodos] = useState([
     {
       id:1,
-      text: "criar funcionalidade x no sistema",
+      text: "Criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
@@ -27,15 +29,64 @@ function App() {
     }
   ]);
 
+  // Pesquisa tarefas, Filtragem e Ordenação
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
+  // Cadastra novas tarefas | se dar o reload na página, os dados somem
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, 
+      {
+        id: Math.floor(Math.random() * 1000),
+        text, 
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  // Deleta as tarefas com base no id
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodo = newTodos.filter(todo => todo.id !== id ? todo : null);
+    setTodos(filteredTodo);
+  };
+
+  // Completa a tarefa | muda o estilo/css
+  const completTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      {/* Pesquisa de Tarefas */}
+      <Search search={search} setSearch={setSearch}/>
+
+      {/* Filtragem e Ordenação */}
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+
+      {/* Lista com as tarefas */}
       <div className="todo_list">
-        {todos.map((todo) => (
-          <Todo todo = {todo}/>
-        ))}
+        {todos
+        // Filtragem por status | Todas, Completas e Incompletas
+        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+        // Ordenação por ordem alfabetica
+        .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+        // Busca por tarefas | utiliza um input de pesquisa
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        // Mapea todas as tarefas criando a chave única (id)
+        .map((todo) => (<Todo key={todo.id} todo = {todo} removeTodo={removeTodo} completTodo={completTodo}/>))
+        }
       </div>
-      <TodoForm/>
+
+      {/* Formulário de cadastro */}
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
--- to_do_react/src/components/TodoForm.jsx
+++ to_do_react/src/components/TodoForm.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import { useState } from 'react';
 
-const TodoForm = () => {
-  return(
-    <div className='tdo`form'>
+const TodoForm = ({ addTodo }) => {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title || !category) return;
+
+    // Adicionando os valores
+    addTodo(title, category);
+
+    // Limpar os campos
+    setTitle("");
+    setCategory("");
+
+  };
+
+  return (
+    <div className='todo-form'>
       <h2>Criar Tarefa</h2>
-      <form>
-        <input type="text" placeholder='Digite o título da tarefa'/>
-        <select>
-            <option value="">Selecione uma categoria</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Casa">Casa</option>
-            <option value="Escola">Escola</option>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Digite o título da tarefa" value={title} onChange={(e) => setTitle(e.target.value)}/>
+
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Casa">Casa</option>
+          <option value="Escola">Escola</option>
         </select>
-        <button type='submit'>Cria tarefa</button>
+        <button type='submit'>Criar tarefa</button>
       </form>
     </div>
-    );
-  
+  );
 };
 
-export default TodoForm
+export default TodoForm;
